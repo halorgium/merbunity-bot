45f27b91802c1c49d824ba7627b7da5cde819b5e
Tim Carey-Smith <tim@spork.in>
2008-06-10 01:26:05 +1200

    Fix the bot and add a broadcaster example

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,8 +1,10 @@
 require 'rubygems'
-require 'xmpp4r'
 require 'logger'
 require 'yaml'
 require 'json'
+require 'set'
+require 'xmpp4r'
+require 'xmpp4r/roster'
 
 $:.unshift File.dirname(__FILE__)
 
@@ -19,69 +21,82 @@ class Bot
   end
   attr_reader :config_filename
 
-  def connect
-    @stream = Jabber::Client.new(config["jid"])
-    @stream.connect
-    @stream.auth(config["pass"])
-
-    @stream.on_exception do |*a|
-      disconnect
+  def start
+    @client = Jabber::Client.new(config["jid"])
+    @client.on_exception do |*a|
+      logger.error "Got a client exception: #{a.inspect}"
+      connect
     end
-
-    @stream.add_presence_callback do |p|
+    @client.add_presence_callback do |p|
       handle_presence(p)
     end
-    @stream.add_message_callback do |m|
+    @client.add_message_callback do |m|
       handle_message(m)
     end
+    connect
 
-    pres = Jabber::Presence.new
-    @stream.send(pres)
-
-    loop { sleep 1 }
+    Thread.stop
   end
 
+  def connect
+    @client.connect
+    @client.auth(config["pass"])
+    @client.send(Jabber::Presence.new)
+    @roster ||= Jabber::Roster::Helper.new(@client)
+  end
+
+
   def handle_presence(pres)
-    if pres.type == :subscribe
+    return if pres.from.bare == @client.jid.bare
+    case pres.type
+    when :subscribe
       logger.info "Got a subscription request from #{pres.from}"
-      reply = Jabber::Presence.new
-      reply.type = :subscribe
-      reply.to = pres.from
-      @stream.send(reply)
-      reply = Jabber::Presence.new
-      reply.type = :subscribed
-      reply.to = pres.from
-      @stream.send(reply)
+      @roster.accept_subscription(pres.from)
+    when :unsubscribe
+      logger.info "Got an unsubscription request from #{pres.from}"
+      Thread.new {
+        if @roster[pres.from].remove
+          logger.info "#{pres.from} removed from the roster"
+        else
+          logger.warn "#{pres.from} not removed from the roster"
+        end
+      }
     end
   end
 
   def handle_message(msg)
-    logger.debug "msg: #{msg.inspect}"
+    logger.debug "Handling message: #{msg.inspect}"
     return if msg.body.nil?
     return unless controlling_jid?(msg.from)
-    deliver_message(msg.body)
+    parse_command(msg.from, msg.body)
   end
 
-  def deliver_message(body)
-    command = JSON.parse(body)
-    command["rcpts"].each do |rcpt|
+  def parse_command(from, command_string)
+    command = JSON.parse(command_string)
+    rcpts = command["rcpts"] || online_watchers
+
+    logger.info "Delivering #{command["body"]} to #{rcpts.inspect}"
+
+    rcpts.each do |rcpt|
       msg = Jabber::Message.new(rcpt, command["body"])
       msg.type = :chat
-      @stream.send(msg)
+      @client.send(msg)
     end
   rescue
-    logger.error "Failed to deliver message: #{body.inspect}"
+    logger.error "Failed to parse command: #{command_string.inspect} with #{$!.class}, #{$!.message}"
+  end
+
+  def online_watchers
+    watchers = []
+    @roster.items.each do |jid,item|
+      watchers << jid if item.online?
+    end
+    watchers
   end
 
   def controlling_jid?(jid)
-    logger.debug "testing jid: #{jid.bare.to_s.inspect}"
+    logger.debug "Checking if #{jid.bare} is a controller"
     config["controllers"].include?(jid.bare.to_s)
-  end
-
-  def disconnect
-    @stream.close if @stream
-  rescue Exception => e
-    logger.error "Failed to disconnect cleanly: #{e.class}, #{e.message}"
   end
 
   def config
@@ -92,11 +107,10 @@ end
 begin
   Bot.logger.debug "Starting on #{$$}"
   b = Bot.new(ARGV.first)
-  b.connect
+  b.start
 rescue Exception => e
   Bot.logger.error "Caught exception: #{e.class}, #{e.message}"
   Bot.logger.debug e.backtrace.join("\n")
-  b.disconnect if b
 end
 
 Bot.logger.debug "Finishing on #{$$}"
